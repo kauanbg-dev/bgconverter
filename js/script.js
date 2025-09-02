b20bc4470114bc91045089b1317f9ff28ea81d2b
Kauan Borges <kauanbg.dev@gmail.com>
2025-09-02 13:04:41 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+const form  = document.getElementById('form');
+form.addEventListener('submit', handleSubmit)
+
+const inputValue = document.getElementById('value-real');
+const selectedCurrency = document.getElementById('currency');
+const result = document.getElementById('result');
+let valueconvert = 0;
+
+function handleSubmit(e) {
+    e.preventDefault();
+
+    if(!inputValue.value || inputValue.value < 0) {
+        alert('informe um valor!');
+        return
+    } else if(!selectedCurrency.value) {
+        alert('Escolha uma moeda!')
+        return;
+    }
+
+    converter();
+};
+
+function converter () {
+    if(selectedCurrency.value === 'eur'){
+        valueConverted = inputValue.value / 6.37;
+        result.innerHTML = valueFormatter('pt-BR', 'EUR');
+
+        animateResult();
+    } else if(selectedCurrency.value === 'dol') {
+        valueConverted = inputValue.value / 5.44 ;
+        result.innerHTML = valueFormatter('en-US', 'USD');
+        
+        animateResult();
+    }
+
+    inputValue.value = '';
+    selectedCurrency.value = '';
+};
+
+function valueFormatter(locale, currency) {
+    const value = valueConverted.toLocaleString(`${locale}` , { style: 'currency' , currency: `${currency}` });
+    return `<span>💸</span> ${value} <span>💸</span>`;
+};
+
+function animateResult() {
+    return result.animate([
+        {  transform: 'translateY(-150px)' },
+        {  transform: 'translateY(0px)' },
+    ], {duration: 400 }); 
+}
